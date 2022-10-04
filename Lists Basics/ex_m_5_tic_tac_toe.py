5b82624918c58bc592c5acb4dd735f5bf0773429
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2022-10-04 19:16:46 +0300

    Tasks

--- Lists Basics/ex_m_5_tic_tac_toe.py
+++ Lists Basics/ex_m_5_tic_tac_toe.py
@@ -1,38 +1,54 @@
 first_line = input().split()
 second_line = input().split()
 third_line = input().split()
-nr_of_zeroes = 0
-nr_of_ones = 0
-nr_of_twos = 0
+first_player_wins = False
+second_player_wins = False
 
-for nr in range(len(first_line)):
-    if first_line[nr] == "1":
-        nr_of_ones += 1
-    elif first_line[nr] == "2":
-        nr_of_twos += 1
-    else:
-        nr_of_zeroes += 1
-for nr in range(len(second_line)):
-    if second_line[nr] == "1":
-        nr_of_ones += 1
-    elif second_line[nr] == "2":
-        nr_of_twos += 1
-    else:
-        nr_of_zeroes += 1
-for nr in range(len(third_line)):
-    if third_line[nr] == "1":
-        nr_of_ones += 1
-    elif third_line[nr] == "2":
-        nr_of_twos += 1
-    else:
-        nr_of_zeroes += 1
-
-if nr_of_ones >= 3 or nr_of_twos >= 3:
-    if nr_of_ones > nr_of_twos:
-        print("First player won")
-    elif nr_of_twos > nr_of_ones:
-        print("Second player won")
-    else:
-        print("Draw!")
+if first_line[0] == first_line[1] == first_line[2]:
+    if first_line[0] == "1":
+        first_player_wins = True
+    elif first_line[0] == "2":
+        second_player_wins = True
+elif second_line[0] == second_line[1] == second_line[2]:
+    if second_line[0] == "1":
+        first_player_wins = True
+    elif second_line[0] == "2":
+        second_player_wins = True
+elif third_line[0] == third_line[1] == third_line[2]:
+    if third_line[0] == "1":
+        first_player_wins = True
+    elif third_line[0] == "2":
+        second_player_wins = True
+elif first_line[0] == second_line[0] == third_line[0]:
+    if first_line[0] == "1":
+        first_player_wins = True
+    elif first_line[0] == "2":
+        second_player_wins = True
+elif first_line[1] == second_line[1] == third_line[1]:
+    if first_line[1] == "1":
+        first_player_wins = True
+    elif first_line[1] == "2":
+        second_player_wins = True
+elif first_line[2] == second_line[2] == third_line[2]:
+    if first_line[2] == "1":
+        first_player_wins = True
+    elif first_line[2] == "2":
+        second_player_wins = True
+elif first_line[0] == second_line[1] == third_line[2]:
+    if first_line[0] == "1":
+        first_player_wins = True
+    elif first_line[0] == "2":
+        second_player_wins = True
+elif first_line[2] == second_line[1] == third_line[0]:
+    if first_line[2] == "1":
+        first_player_wins = True
+    elif first_line[2] == "2":
+        second_player_wins = True
+if first_player_wins:
+    print("First player won")
+elif second_player_wins:
+    print("Second player won")
+else:
+    print("Draw!")
 
 
