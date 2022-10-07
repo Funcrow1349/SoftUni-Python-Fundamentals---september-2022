5b2706216fa2046eb7c8e0c522550b49622d39e4
Ivaylo Ivanov <113984399+Funcrow1349@users.noreply.github.com>
2022-10-07 10:59:11 +0200

    Project

---
+++ Functions/basic_calculator.py
@@ -0,0 +1,61 @@
+# basic calculator
+def intro():
+    return '''##################################
+# Welcome to SoftUni Calculator  #
+#   Please select an option to   #
+#          continue              #
+#                                #
+#                                #
+#   Author: Ivaylo Ivanov        #
+#   Course: SoftUni Fundamentals #
+#                                #
+##################################\n'''
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+print(intro())
+
+while True:
+    choice = input('1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit\nEnter your choice: ')
+
+    if choice == '5':
+        print('Thank you for using our calculator!!!')
+        break
+
+    if choice == '1':
+        n1 = float(input('Enter first number: '))
+        n2 = float(input('Enter second number: '))
+        print('The sum is: ', add(n1, n2), '\n')
+
+    elif choice == '2':
+        n1 = float(input('Enter first number: '))
+        n2 = float(input('Enter second number: '))
+        print('The result of the subtract is: ', subtract(n1, n2), '\n')
+
+    elif choice == '3':
+        n1 = float(input('Enter first number: '))
+        n2 = float(input('Enter second number: '))
+        print('The sum is: ', multiply(n1, n2), '\n')
+
+    elif choice == '4':
+        n1 = float(input('Enter first number: '))
+        n2 = float(input('Enter second number: '))
+        print('The sum is: ', divide(n1, n2), '\n')
+
+    else:
+        print('Wrong choice!!!\n')
