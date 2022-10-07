162da23a54420fee66f5d51cd496a38fa23542ef
Ivaylo Ivanov <113984399+Funcrow1349@users.noreply.github.com>
2022-10-07 11:03:08 +0200

    Project

--- Functions/t7_rounding.py
+++ Functions/t7_rounding.py
@@ -4,3 +4,8 @@ for num in numbers:
     rounded_numbers.append(round(num))
 
 print(rounded_numbers)
+
+# # 7.	Rounding
+# result = list(map(lambda x: round(float(x)), input().split(' ')))
+#
+# print(result)
