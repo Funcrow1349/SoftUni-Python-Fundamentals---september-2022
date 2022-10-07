8fd09659f48b6aad0b62e0bceb5ed73cd8295d11
Ivaylo Ivanov <113984399+Funcrow1349@users.noreply.github.com>
2022-10-07 12:01:16 +0200

    Tasks

---
+++ Functions/ex_2_add_and_subtract.py
@@ -0,0 +1,14 @@
+def add_and_subtract():
+    first_number = int(input())
+    second_number = int(input())
+    third_number = int(input())
+
+    def sum_numbers(first_number, second_number):
+        return first_number + second_number
+
+    def subtract(sum_numbers, third_number):
+        return sum_numbers - third_number
+
+
+print(subtract)
+
