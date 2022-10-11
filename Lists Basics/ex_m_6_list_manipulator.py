279f80d4b969dc12acf3f04322fb9d622a3fc515
Ivaylo Ivanov <113984399+Funcrow1349@users.noreply.github.com>
2022-10-11 14:25:57 +0200

    Tasks

--- Lists Basics/ex_m_6_list_manipulator.py
+++ Lists Basics/ex_m_6_list_manipulator.py
@@ -1,135 +1,154 @@
 import sys
 
-initial_entry = input().split()
+initial_input = input().split()
 line_of_integers = []
-for string in initial_entry:
-    line_of_integers.append(int(string))
+for sym in initial_input:
+    line_of_integers.append(int(sym))
 current_command = input()
-
 while current_command != "end":
     if "exchange" in current_command:
         current_command = current_command.split()
         index_ex = int(current_command[1])
         mutated_list = []
-        if int(current_command[1]) < 0 or int(current_command[1]) >= len(line_of_integers):
-            print("Invalid index")
+        invalid_index = False
+        if index_ex >= len(line_of_integers) or index_ex < 0:
+            invalid_index = True
         else:
             for a in range(index_ex + 1, len(line_of_integers)):
                 mutated_list.append(line_of_integers[a])
             for b in range(0, index_ex + 1):
                 mutated_list.append(line_of_integers[b])
-        line_of_integers = mutated_list
-    elif "max" in current_command:
-        no_matches = True
-        if "odd" in current_command:
-            max_odd_index = 0
-            max_odd_number = -sys.maxsize
-            for c in range(len(line_of_integers)):
-                if line_of_integers[c] % 2 != 0 and line_of_integers[c] >= max_odd_number:
-                    max_odd_index = c
-                    max_odd_number = line_of_integers[c]
-                    no_matches = False
-                else:
-                    continue
-            if not no_matches:
-                print(max_odd_index)
-        elif "even" in current_command:
-            max_even_index = 0
-            max_even_number = -sys.maxsize
-            for d in range(len(line_of_integers)):
-                if line_of_integers[d] % 2 == 0 and line_of_integers[d] >= max_even_number:
-                    max_even_index = d
-                    max_even_number = line_of_integers[d]
-                    no_matches = False
-                else:
-                    continue
-            if not no_matches:
-                print(max_even_index)
-        if no_matches:
+        if invalid_index:
+            print("Invalid index")
+        else:
+            line_of_integers = mutated_list
+    elif "max" in current_command and "odd" in current_command:
+        max_odd_number = -sys.maxsize
+        index_max_odd_number = 0
+        max_odd_matches = False
+        for num in line_of_integers:
+            if num % 2 != 0 and num >= max_odd_number:
+                max_odd_number = num
+                index_max_odd_number = line_of_integers.index(num)
+                max_odd_matches = True
+        if not max_odd_matches:
             print("No matches")
-    elif "min" in current_command:
-        no_matches = True
-        if "odd" in current_command:
-            min_odd_index = 0
-            min_odd_number = sys.maxsize
-            for e in range(len(line_of_integers)):
-                if line_of_integers[e] % 2 != 0 and line_of_integers[e] <= min_odd_number:
-                    min_odd_index = e
-                    min_odd_number = line_of_integers[e]
-                    no_matches = False
-                else:
-                    continue
-            if not no_matches:
-                print(min_odd_index)
-        elif "even" in current_command:
-            min_even_index = 0
-            min_even_number = sys.maxsize
-            for f in range(len(line_of_integers)):
-                if line_of_integers[f] % 2 == 0 and line_of_integers[f] <= min_even_number:
-                    min_even_index = f
-                    min_even_number = line_of_integers[f]
-                    no_matches = False
-                else:
-                    continue
-            if not no_matches:
-                print(min_even_index)
-        if no_matches:
+        else:
+            print(index_max_odd_number)
+    elif "max" in current_command and "even" in current_command:
+        max_even_number = -sys.maxsize
+        index_max_even_number = 0
+        max_even_matches = False
+        for num in line_of_integers:
+            if num % 2 == 0 and num >= max_even_number:
+                max_even_number = num
+                index_max_even_number = line_of_integers.index(num)
+                max_even_matches = True
+        if not max_even_matches:
             print("No matches")
-    elif "first" in current_command:
-        first_list = []
-        if "even" in current_command:
-            current_command = list(current_command)
-            index_first = 0
-            while len(first_list) < int(current_command[6]):
-                if index_first > len(line_of_integers):
+        else:
+            print(index_max_even_number)
+    elif "min" in current_command and "odd" in current_command:
+        min_odd_number = sys.maxsize
+        index_min_odd_number = 0
+        min_odd_matches = False
+        for num in line_of_integers:
+            if num % 2 != 0 and num <= min_odd_number:
+                min_odd_number = num
+                index_min_odd_number = line_of_integers.index(num)
+                min_odd_matches = True
+        if not min_odd_matches:
+            print("No matches")
+        else:
+            print(index_min_odd_number)
+    elif "min" in current_command and "even" in current_command:
+        min_even_number = sys.maxsize
+        index_min_even_number = 0
+        min_even_matches = False
+        for num in line_of_integers:
+            if num % 2 == 0 and num <= min_even_number:
+                min_even_number = num
+                index_min_even_number = line_of_integers.index(num)
+                min_even_matches = True
+        if not min_even_matches:
+            print("No matches")
+        else:
+            print(index_min_even_number)
+    elif "first" in current_command and "odd" in current_command:
+        current_command = current_command.split()
+        first_odd_count = int(current_command[1])
+        first_odd_list = []
+        invalid_count = False
+        for num in line_of_integers:
+            if first_odd_count > len(line_of_integers):
+                invalid_count = True
+                break
+            if num % 2 != 0:
+                first_odd_list.append(num)
+                if len(first_odd_list) == first_odd_count:
                     break
-                if int(current_command[6]) > len(line_of_integers):
-                    break
-                if line_of_integers[index_first] % 2 == 0:
-                    first_list.append(line_of_integers[index_first])
-                index_first += 1
-        elif "odd" in current_command:
-            current_command = list(current_command)
-            index_second = 0
-            while len(first_list) < int(current_command[6]):
-                if index_second > len(line_of_integers):
-                    break
-                if int(current_command[6]) > len(line_of_integers):
-                    break
-                if line_of_integers[index_second] % 2 != 0:
-                    first_list.append(line_of_integers[index_second])
-                index_second += 1
-        if int(current_command[6]) > len(line_of_integers):
+        if invalid_count:
             print("Invalid count")
         else:
-            print(first_list)
-    elif "last" in current_command:
-        last_list = []
-        if "even" in current_command:
-            current_command = list(current_command)
-            index_last = len(line_of_integers) - 1
-            while len(last_list) < int(current_command[5]):
-                if index_last < 0:
+            print(first_odd_list)
+    elif "first" in current_command and "even" in current_command:
+        current_command = current_command.split()
+        first_even_count = int(current_command[1])
+        first_even_list = []
+        invalid_count = False
+        for num in line_of_integers:
+            if first_even_count > len(line_of_integers):
+                invalid_count = True
+                break
+            if num % 2 == 0:
+                first_even_list.append(num)
+                if len(first_even_list) == first_even_count:
                     break
-                if int(current_command[5]) > len(line_of_integers):
-                    break
-                if line_of_integers[index_last] % 2 == 0:
-                    last_list.append(line_of_integers[index_last])
-                index_last -= 1
-        elif "odd" in current_command:
-            current_command = list(current_command)
-            index_mid = len(line_of_integers) - 1
-            while len(last_list) < int(current_command[5]):
-                if index_mid < 0:
-                    break
-                if int(current_command[5]) > len(line_of_integers):
-                    break
-                if line_of_integers[index_mid] % 2 != 0:
-                    last_list.append(line_of_integers[index_mid])
-                index_mid -= 1
-        if int(current_command[5]) > len(line_of_integers):
+        if invalid_count:
             print("Invalid count")
         else:
-            print(last_list)
+            print(first_even_list)
+    elif "last" in current_command and "odd" in current_command:
+        current_command = current_command.split()
+        last_odd_count = int(current_command[1])
+        last_odd_list_reversed = []
+        invalid_count = False
+        for num in range(len(line_of_integers) - 1, -1, -1):
+            if last_odd_count > len(line_of_integers):
+                invalid_count = True
+                break
+            if line_of_integers[num] % 2 != 0:
+                last_odd_list_reversed.append(line_of_integers[num])
+                if len(last_odd_list_reversed) == last_odd_count:
+                    break
+        if invalid_count:
+            print("Invalid count")
+        else:
+            if len(last_odd_list_reversed) >= 1:
+                print(last_odd_list_reversed)
+            else:
+                last_odd_list_reversed.reverse()
+                print(last_odd_list_reversed)
+    elif "last" in current_command and "even" in current_command:
+        current_command = current_command.split()
+        last_even_count = int(current_command[1])
+        last_even_list_reversed = []
+        invalid_count = False
+        for num in range(len(line_of_integers) - 1, -1, -1):
+            if last_even_count > len(line_of_integers):
+                invalid_count = True
+                break
+            if line_of_integers[num] % 2 == 0:
+                last_even_list_reversed.append(line_of_integers[num])
+                if len(last_even_list_reversed) == last_even_count:
+                    break
+        if invalid_count:
+            print("Invalid count")
+        else:
+            if len(last_even_list_reversed) >= 1:
+                print(last_even_list_reversed)
+            else:
+                last_even_list_reversed.reverse()
+                print(last_even_list_reversed)
     current_command = input()
 print(line_of_integers)
---
+++ Lists Basics/test_lists.py
@@ -0,0 +1,2 @@
+some_list = [1, 2, 3, 4, 5]
+print(some_list.reverse())
